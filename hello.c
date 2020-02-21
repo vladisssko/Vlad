7a34b44630296e61b3c5701fddef574df5329a60
vladisssko <vladisssko@users.noreply.github.com>
2020-02-21 00:43:41 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+
+int main(void)
+{
+    
+    string meno = get_string("ako sa volas?\n");
+    printf("ako sa volas ?\n");
+
+    
+
+    printf("ahoj, %s\n", meno);
+
+}
