9f93990e34519bef0b5115dcb99a492144b7524c
vladisssko <vladisssko@users.noreply.github.com>
2020-03-03 12:07:17 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -85,7 +85,9 @@ void print_winner(void)
     for (int i = 0; i<candidate_count; i++ )
         {
             if(candidates[i].votes > maxvotes)
+            {
             maxvotes = candidates[i].votes;
+            }
         }
         
     
