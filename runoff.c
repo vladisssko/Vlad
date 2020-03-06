7709ea18e52d3c05d4bbf207aa3cda768e20ce0e
vladisssko <vladisssko@users.noreply.github.com>
2020-03-06 20:32:05 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -181,11 +181,9 @@ bool print_winner(void)
             if (candidates[i].eliminated == true)
             {
             candidate_count_eliminated--;
-            }
-        
             
             if(candidates[i].votes >= (candidate_count_eliminated / 2) + 0.5) 
-            {
+            
                  printf("%s is the winner\n", candidates[i].name );
                  return true;
             }
