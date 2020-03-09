0c43ac0dc088e24a7d76dabc7c07fcfa24a137be
vladisssko <vladisssko@users.noreply.github.com>
2020-03-09 13:07:39 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -11,7 +11,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
   
     for(int i = 0; i < height-1;i++)
     {
-        for(int j = 0; j < width;j++)
+        for(int j = 0; j < width-1;j++)
         {
         float avarage_decimals = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3;  
         int avarage =  round(avarage_decimals);
