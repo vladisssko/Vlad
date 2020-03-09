f5829003499fa0d815e0f8d3231c5ffa525b9a4b
vladisssko <vladisssko@users.noreply.github.com>
2020-03-09 13:51:23 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,7 +9,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
   
-    for(int i = 0; i <= height ;i++)
+    for(int i = 0; i < height ;i++)
     {
         for(int j = 0; j < width;j++)
         {
