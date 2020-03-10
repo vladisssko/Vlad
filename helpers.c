cb74bb89a583e7bab91652ae22feb1b50a6726d3
vladisssko <vladisssko@users.noreply.github.com>
2020-03-10 18:33:31 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -65,12 +65,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0;i<height;i++)
     {
         for(int j = 0; j< width/2;j++)
-        {
+{
             temp = image[i][j];
             image[i][j] = image[i][width -j-1];
             image[i][width -j-1] = temp;
-        }
-    }
+}
+}
     return;
 }
 
