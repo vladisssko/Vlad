d27f8db37d62e5c026626ea7845d68e2147311c5
vladisssko <vladisssko@users.noreply.github.com>
2020-03-13 18:03:58 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -27,10 +27,12 @@ bool first_jpeg_found = false;
 
 //go trought each chunk of 512 bytes until reaches end of the card 
 while (fread (bytebuffer, 1, 512, inptr) ==512 )
-
+{
 //check if its JPEg header - has defined first 3 bytes = 1st jpeg 
 if (bytebuffer[0] == 0xff && bytebuffer[1] == 0xd8 && bytebuffer[2] == 0xff )
     {
+        if(no_jpeg>1)
+        fclose(image);
        first_jpeg_found = true;
        sprintf(filename, "%03i.jpg",no_jpeg); //prepise filename podla formatu xxx.jpg
        image = fopen(filename, "w"); //otvori xxx.jpg
@@ -46,30 +48,11 @@ if (bytebuffer[0] == 0xff && bytebuffer[1] == 0xd8 && bytebuffer[2] == 0xff )
        if (first_jpeg_found==true) //if header already found for 1st jpeg 
        {
            fwrite(bytebuffer, 1, 512, image); //continues writing in to fist jpeg
+           
        }
        
-       
-       
-       
-        
+}
 
-      
-    
-      
-  
-      
-      
-   
-    
-   
-
-
-        
-    
-
-
-
-
-
+fclose(image);
 
 }
