0f766341e12875e9ac38429dcda9f6eae1f79b9b
vladisssko <vladisssko@users.noreply.github.com>
2020-03-26 17:23:27 +0000

    automated commit by submit50

---
+++ mario.py
@@ -0,0 +1,20 @@
+from cs50 import get_int
+
+def main():
+    
+    i = get_correct_int()
+    for a in range(i):
+        for b in range(i-a-1):
+            print(" ", end ="")
+           
+        for c in range(a+1):
+            print("#", end="")
+        print()
+            
+def get_correct_int():    
+    while True:
+        height = get_int("Height: ")
+        if height>1 and height<9: 
+            break
+    return height
+main()   
